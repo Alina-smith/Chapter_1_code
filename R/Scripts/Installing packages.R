af40ba8d0d75217d245f1264fbdee70a2739e91a
Alina-smith <absmith4@sheffield.ac.uk>
2024-08-10 03:32:19 +0100

    WOS edits

---
+++ R/Scripts/Installing packages.R
@@ -0,0 +1,8 @@
+## script to install packages when R needs to be updated
+install.packages("tidyr")
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("stringi")
+install.packages("taxize")
+install.packages("ggplot2")
+install.packages("here")
