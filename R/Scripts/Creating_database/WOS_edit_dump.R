1a38088c5585bff484ccabe792ee0c79568aa8af
Alina-smith <absmith4@sheffield.ac.uk>
2024-08-15 23:53:12 +0100

    WOS edits - adding in body mass results and changing structure of sources_list sheet

--- R/Scripts/Creating_database/WOS_edit_dump.R
+++ R/Scripts/Creating_database/WOS_edit_dump.R
@@ -1,9 +1,9 @@
 ## not their data edits
 
 # data
-four <- read.csv("not_their_data/4.csv")
-seventeen <- read.csv("not_their_data/17.csv")
-two51 <- read.csv("not_their_data/251.csv")
+four <- read.csv("Raw_data/not_their_data/4.csv")
+seventeen <- read.csv("Raw_data/not_their_data/17.csv")
+two51 <- read.csv("Raw_data/not_their_data/251.csv")
 
 # 4
 # select unique papers and get doi from ones that have it
@@ -30,6 +30,7 @@ write.csv(four_edit, "not_their_data/4_edit.csv")
 # 17
 # select unique papers and get doi from ones that have it
 seventeen_1 <- seventeen %>% 
+  distinct(References) %>% 
   mutate(
     doi = stri_extract_first_regex(References, "(?<=doi:)\\S+") # select any non white spaces after the regex "doi: "
   ) 
@@ -50,15 +51,24 @@ write.csv(seventeen_edit, "not_their_data/17_edit.csv")
 
 ## 251
 two51_edit <- two51 %>% 
+  filter(
+    Habitat.Location %in% c("Yeniseher Lake", "Keban dam lake (Turkey)", "Southeastern Lake Michigan (MI, USA)", "Green Mountain Reservoir (Colorado)", "Lake Dillon (Colorado)", "Lake Aquatina, Italy")
+  ) %>% 
   distinct(Source)
+# save
+write.csv(two51_edit, "not_their_data/251_edit.csv")
 
 
 
+full_list <- read.csv("Raw_data/WOS_traits_list.csv")
+shortlist <- read.csv("Raw_data/shortlist_list.csv") %>% 
+  rename(
+    Article.Title = title
+  )
 
-
-
-
-
+new_list <- left_join(shortlist, select(full_list, "Article.Title", "Author.Full.Names", "ISSN", "eISSN", "ISBN", "Book.DOI", "UT..Unique.WOS.ID."), by = "Article.Title")
+# save
+write.csv(new_list, "new_list.csv")
 
 
 
