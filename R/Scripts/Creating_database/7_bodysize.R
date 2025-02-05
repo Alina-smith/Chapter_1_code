0412a942c7b03a89b7fd4647ca8fee5a083d3fd9
Alina-smith <116362297+Alina-smith@users.noreply.github.com>
2025-02-05 18:30:01 +0000

    Finished species database, just need to add in functional traits but have the expert reynolds functional groups in there

--- R/Scripts/Creating_database/7_bodysize.R
+++ R/Scripts/Creating_database/7_bodysize.R
@@ -3,8 +3,6 @@
 # all length, width and height = um,
 # all mass = ug
 
-# just taking the phytoplankton now
-
 # Packages 
 library(here)
 library(readxl)
@@ -15,19 +13,25 @@ library(data.table)
 
 # Data ----
 bodysize_location <- readRDS("R/Data_outputs/full_database/bodysize_location.rds")
+bodysize_taxonomy <- readRDS("R/Data_outputs/full_database/bodysize_taxonomy.rds")
 tax_list <- readRDS("R/Data_outputs/taxonomy/gbif/tax_list.rds")
+source_list <- readRDS("R/Data_outputs/locations_sources/source_list.rds")
+kruk <- read_xlsx("raw_data/kruk_groups.xlsx")
+updated_spec_char <- read_xlsx(here("raw_data","manual_taxonomy.xlsx"), sheet = "special_characters")
 
-# Final all raw list including phyto and zooplankton ----
+# All data ----
+# Final list of all data including phyto and zooplankton
 
-# make a list of all individual uids that has more than one measurement type to use in next step to remove datapoints I don't want
 format_all_bs <- bodysize_location %>% 
   
   mutate(
     
+    ## Small misc edits ----
+    
     # change types for later merging
     uid = as.character(uid),
     
-    # change measurement type to single when its raw but only has one value
+    # change measurement type to single when its raw but only has one value because going to get averages of raw data later on so don't want these ones in there
     measurement.type = if_else(
       uid %in% c("340977", "341039", "343115", "338767", "338672", "338649"),
       "single",
@@ -40,16 +44,19 @@ format_all_bs <- bodysize_location %>%
       "adult",
       life.stage
     ),
-    # First convert all units to be the same to make it easier
+    
+    # Convert all units to be the same to make it easier
     body.size = case_when(
       units %in% c("mg", "mm") ~ body.size*1000,
       TRUE ~ body.size
       ),
+    
     units = case_when(
       units == "mg" ~ "µg",
       units == "mm" ~ "µm",
       TRUE ~ units
       ),
+    
     # set form number for missing one
     form = if_else(
       uid == "321726",
@@ -63,19 +70,20 @@ format_all_bs <- bodysize_location %>%
     - units
   ) %>% 
   
-  # make nu column and rename form.no
+  ## nu column ----
+  # rename form and form.no columns to nu and set so either individual or multi-cellular
   rename(
-    ind.per.nu = form.no
+    ind.per.nu = form.no,
+    nu = form
   ) %>% 
   
   mutate(
-  
     nu = case_when(
       ind.per.nu > 1 ~ "multi-cellular",
       ind.per.nu == 1 ~ "individual",
       
-      form %in% c("multi-cell", "multi-cellular", "colony", "filament") ~ "multi-cellular",
-      form == "individual" ~ "individual",
+      nu %in% c("multi-cell", "multi-cellular", "colony", "filament") ~ "multi-cellular",
+      nu == "individual" ~ "individual",
       TRUE ~ NA
     )
   ) %>% 
@@ -87,11 +95,12 @@ format_all_bs <- bodysize_location %>%
     nu, ind.per.nu, body.size, bodysize.measurement, reps, sample.size, error, error.type, measurement.type,
     tax.uid, rank, species, genus, family, order, class, phylum, kingdom,
     sample.year, sample.month, location.code, habitat, location, country, continent, latitude, longitude,
-    bodysize.measurement.notes, form
+    bodysize.measurement.notes
   )
 
+# Convert raw to averages ----
 
-# get location info to left join to bw_raw
+## get location info to left join to bs_raw ----
 location_info <- format_all_bs %>% 
   select(
     location.code, habitat, location, country, continent, latitude, longitude
@@ -101,17 +110,20 @@ location_info <- format_all_bs %>%
     location.code, .keep_all = TRUE
   )
 
-# Turn raw into averages
+## Calculate average ----
 bs_raw <- format_all_bs %>% 
   
+  # Select raw data
   filter(
     measurement.type == "raw"
-  )%>% 
+  ) %>% 
   
+  # Group by all the below factors so that it gives average for different locations and dates
   group_by(
     tax.uid, source.code, original.sources, location.code, bodysize.measurement, life.stage, sex, sample.year, sample.month, nu
   ) %>% 
   
+  # Calculate mean
   summarise(
     body.size.mean = mean(body.size),
     sample.size = n(),
@@ -126,6 +138,7 @@ bs_raw <- format_all_bs %>%
   
   ungroup() %>% 
   
+  # Join back in extra info
   left_join(
     location_info, by = "location.code"
   ) %>% 
@@ -134,14 +147,13 @@ bs_raw <- format_all_bs %>%
     tax_list, by = "tax.uid"
   ) %>% 
   
-   # add new individual.uid
-  
   left_join(
     select(
       source_list, doi, new.source.code
     ), by = c("source.code" = "new.source.code")
   ) %>% 
   
+  # Make new individual.uid
   group_by(
     tax.uid, source.code, original.sources, location.code, life.stage, sex, sample.year, sample.month, nu
   ) %>% 
@@ -153,6 +165,7 @@ bs_raw <- format_all_bs %>%
   ungroup() %>% 
   
   mutate(
+    # Add in misc extra info
     uid = paste("r", row_number(), sep = "-"),
     ind.per.nu = 1, # all individuals
     reps = NA,
@@ -168,411 +181,281 @@ bs_raw <- format_all_bs %>%
   
   ungroup() %>% 
   
+  # Select and relocate
   select(
     uid, individual.uid, source.code, original.sources, type, life.stage, sex, taxa.name,
     nu, ind.per.nu, body.size, bodysize.measurement, reps, sample.size, error, error.type, measurement.type,
     tax.uid, rank, species, genus, family, order, class, phylum, kingdom,
     sample.year, sample.month, location.code, habitat, location, country, continent, latitude, longitude,
-    bodysize.measurement.notes, form
+    bodysize.measurement.notes
   )
 
-# Combine all together
+## Combine all together ----
 all_bodysize <- format_all_bs %>% 
   
+  # remove raw data
   filter(
     !(measurement.type == "raw")
   ) %>% 
   
+  # add in the new averages of raw data
   bind_rows(
     bs_raw
   )
 
-# save
+## save ----
 saveRDS(all_bodysize, file = "R/Data_outputs/full_database/all_bodysize.rds")
 
 
-# Just phyto ----
+# Phyto ----
+# select just the phytoplankton and add in the kruk groups
 
-x <- all_bodysize %>% 
-  distinct(
-    life.stage
-  )
-
-cell_volume <- all_bodysize %>% 
+## Update names in the kruk data ----
+kruk_names_list <- bodysize_taxonomy %>% # used bodysize_taxonomy because it has the original.taxa.names in
   
+  # select just kruk data
   filter(
-    type == "Phytoplankton",
-    life.stage %in% c("adult", "active")
-  ) %>% 
-  filter(
-    nu == "multi-cellular"
-  )
-%>% 
-  pivot_wider(
-    id_cols = individual.uid,
-    names_from = nu,
-    values_from = body.size
-  ) %>% 
-  unnest()
-
-
-
-
-
-
-
-
-
-
-
-
-# Make a column for individual and nu body size
-ind.body.size = case_when(
-  nu == "individual" ~ body.size,
-  nu == "multi-cellular" & !is.na(ind.per.nu) ~ body.size/ind.per.nu,
-  nu == "multi-cellular" & is.na(ind.per.nu) ~ NA,
-  TRUE ~ NA
-),
-
-nu.size = body.size
-
-
-
-  
-  
-cell_volume <- format_all_bs %>% 
-  
-  filter(
-    !(measurement.type == "raw")
+    source.code == "1313"
   ) %>% 
   
-  # remove colum ns not relevent for phytoplankton
+  # select name columns
   select(
-    - life.stage,
-    - sex
-  )
-
-# save
-saveRDS(all_bodysize, file = "R/Data_outputs/full_database/all_bodysize.rds")
-
-
-raw_size <- all_bodysize %>% 
-  filter(
-    type == "Phytoplankton",
-    measurement.type == 
-  )
-
-
-
-cell_volume <- all_bodysize %>% 
+    original.taxa.name,
+    taxa.name
+  ) %>%
   
-  filter(
-    type == "Phytoplankton",
-    life.stage != "dormant" # keep just active ones
+  # get distinct names
+  distinct(
+    original.taxa.name, .keep_all = TRUE
+  ) 
+
+kruk_groups <- kruk %>% 
+  
+  # select columns
+  select(
+    Species_name,
+    `Classification by Experts`
   ) %>% 
   
-  # rename nu columns to cells
   rename(
-    cells.per.nu = ind.per.nu,
-    cell.size = ind.body.size,
+    original.taxa.name = Species_name,
+    expert.reynolds = `Classification by Experts`
   ) %>% 
   
-  group_by(
-    uid, tax.uid, source.code, original.sources, location.code, bodysize.measurement
+  # Need to edit the names to remove species characters as done in join_db script so that the original.taxa.names are the same for left joining
+  mutate(
+    # remove any random capitals - set just first letter to upper case, gna_verify doesn't work with anything else
+    original.taxa.name = tolower(original.taxa.name), # set all to lower case
+    original.taxa.name = paste(
+      toupper(
+        str_sub(
+          original.taxa.name, 1,1 # select first letter and set to upper case
+        )
+      ),
+      str_sub(original.taxa.name, 2), # paste remaining word
+      sep = ""
+    ),
+    
+    # add in *SpecChar* to replace special characters
+    original.taxa.name = stri_replace_all_regex(original.taxa.name, "[^\\x20-\\x7E]", "*SpecChar*"),
   ) %>% 
   
-  summarise(
-    individual.body.size = mean(cell.size),
-    multicellular.body.size = mean(nu.size),
-    #uid = cur_group_id()
-  ) %>% 
-  
-  ungroup() 
-%>% 
-  
-  group_by(
-    tax.uid, source.code, original.sources, location.code
+  # Join updated names
+  left_join(
+    updated_spec_char, by = "original.taxa.name"
   ) %>% 
   
   mutate(
-    uid = cur_group_id()
+    # replace old with new
+    original.taxa.name = if_else(
+      !is.na(new.taxa.name), new.taxa.name, original.taxa.name
+    ),
+    
+    # Remove any white spaces if there are any
+    original.taxa.name = trimws(original.taxa.name)
   ) %>% 
   
-  ungroup()
-
-
-
-
-
-
-
-%>% 
+  # remove redundant columns
+  select(
+    - new.taxa.name
+  ) %>% 
   
+  # add in the new names
+  left_join(kruk_names_list, by = "original.taxa.name") %>% 
+  
+  # remove original.taxa.name
+  select(-original.taxa.name) %>% 
+  
+  # get only distinct non NA names
+  distinct(
+    taxa.name, .keep_all = TRUE
+  ) %>% 
+  
+  filter(
+    !is.na(taxa.name)
+  )
+
+
+phyto <- all_bodysize %>% 
+  
+  mutate(
+    # set depth in bodysize.measurment as height as there is no overlap between these
+    bodysize.measurement = if_else(
+      bodysize.measurement == "depth",
+      "height",
+      bodysize.measurement
+    )
+  ) %>% 
+  
+  # Select data I want
+  filter(
+    type == "Phytoplankton",
+    life.stage %in% c("adult", "active") # select just active and not dormant
+  ) %>% 
+  
+  # Rename from body to cell
+  rename(
+    cell.size = body.size,
+    cells.per.nu = ind.per.nu
+  ) %>% 
+  
+  mutate(
+    nu = if_else(
+      nu == "individual",
+      "cell",
+      nu
+    )
+  ) %>% 
+  
+  select(
+    # don't need life.stage and sex for phyto as all active and don't have sex info
+    - life.stage,
+    - sex,
+    
+    # don't have error data for majority so leaving it out
+    - error,
+    - error.type,
+    - reps,
+    - sample.size,
+    
+    - measurement.type, # all average now
+    - bodysize.measurement.notes, # not needed
+    - type # all phyto
+  ) %>% 
+  
+  left_join(
+    kruk_groups, by = "taxa.name"
+  )
+
+# Cell mass ----
+# Calculate cell mass
+
+## Separate volumes ----
+# find the mld for each individual and calculate body.mass
+
+species_raw_cell_size <- phyto %>% 
+  
+  # Get all the different bodysize.measurements on one row per individual
   pivot_wider(
     id_cols = individual.uid,
     names_from = bodysize.measurement,
-    values_from = c(cell.size, nu.size)
+    values_from = cell.size
   ) %>%
   
-  unnest() %>% 
-  
   rename(
-    cell.volume = cell.size_biovolume,
-    cell.length = cell.size_length,
-    cell.width = cell.size_width,
-    cell.height = cell.size_height,
-    cell.depth = cell.size_depth,
-    cell.diameter = cell.size_diameter,
-    cell.dry.mass = `cell.size_dry mass`,
-    
-    nu.volume = nu.size_biovolume,
-    nu.length = nu.size_length,
-    nu.width = nu.size_width,
-    nu.height = nu.size_height,
-    nu.depth = nu.size_depth,
-    nu.diameter = nu.size_diameter,
-    nu.dry.mass = `nu.size_dry mass`,
-  )
-
-
-
-
-
-
+    dry.mass = `dry mass`
+  ) %>% 
   
-  # calculate mass from biovolume
-  mutate(
-    cell.mass = case_when(
-      bodysize.measurement == "biovolume" ~ cell.size*(1*10^-6),
-      TRUE ~ NA
-    )
-  )
-
-x <- cell_volume %>% 
-  filter(
-    is.na(cell.mass)
-  )
-
-mass = volume*(1*10^-6)
-
-x <- cell_volume %>% 
+  # join all extra data back 
+  left_join(
+    select(
+      phyto, - cell.size, bodysize.measurement, - uid
+      ), by = "individual.uid"
+  ) %>% 
+  
   distinct(
-    bodysize.measurement
-  )
-
-
-
-%>% 
-    
-    # make two columns for nu (multiple cells) and individuals
-    # some are written wrong in the raw data so just covering all variations to be sure
-    
-    cell.size = case_when(
-      nu == "individual" ~ body.size,
-      nu == "multi-cellular" & !is.na(cells.per.nu) ~ body.size/cells.per.nu,
-      nu == "multi-cellular" & is.na(cells.per.nu) ~ NA,
+    individual.uid, .keep_all = TRUE
+  ) %>% 
+  
+  # calculate mass from biovolume and dry mass
+  mutate(
+    mass = case_when(
+      !is.na(biovolume) ~ biovolume*(1*10^-6),
+      is.na(biovolume) & !is.na(dry.mass) ~ dry.mass/0.2,
       TRUE ~ NA
     ),
     
-    nu.size = body.size
-  )
-  
-
-
-
-
-%>% 
-  
-  pivot_wider(
-    id_cols = individual.uid,
-    names_from = bodysize.measurement,
-    values_from = c(body.size)
-  ) %>%
-  
-  unnest()
-      
-  
-  # get average for each source for taxa at each location for each measurement type - means all sources will be averages now
-  
-  group_by(
-    uid, tax.uid, source.code, original.sources, location.code, bodysize.measurement
+    # make a mld column
+    mld = pmax(length, width, height, diameter, na.rm = TRUE)
   ) %>% 
   
-  summarise(
-    individual.body.size = mean(cell.size),
-    multicellular.body.size = mean(nu.size),
-    #uid = cur_group_id()
-  ) %>% 
-  
-  ungroup() %>% 
-  
-  group_by(
-    tax.uid, source.code, original.sources, location.code
-  ) %>% 
-  
-  mutate(
-    uid = cur_group_id()
-  ) %>% 
-  
-  ungroup()
-
-%>% 
-  
-  pivot_wider(
-    id_cols = uid,
-    names_from = bodysize.measurement,
-    values_from = c(body.size)
-  )
-  
-
-
-# add in rest of data
-# get distinct location codes
-dist_loc_code <- bodysize_location %>% 
+  # select columns and reorder
   select(
-    location.code, latitude, longitude, location, country, continent, habitat
-  ) %>% 
-  distinct(
-    location.code, .keep_all = TRUE
-  )
-  
-body_size <- average_bodysize %>% 
-
-  left_join(
-    select(
-      tax_list, - resolved.taxa.name),
-    by = "tax.uid"
-    ) %>% 
-  
-  left_join(
-      dist_loc_code,
-      by = "location.code"
-    ) %>% 
-  mutate(
-    uid = row_number()
+    individual.uid, source.code, original.sources, taxa.name,
+    expert.reynolds,
+    nu, cells.per.nu, mass, biovolume, mld,
+    tax.uid, rank, species, genus, family, order, class, phylum, kingdom,
+    sample.year, sample.month, location.code, habitat, location, country, continent, latitude, longitude
   ) %>% 
   
-  pivot_wider(
-    id_cols = uid,
-    names_from = bodysize.measurement,
-    values_from = c(individual.body.size, multicellular.body.size)
-  ) %>% 
-  
-  rename(
-    ind.volume = individual.body.size_biovolume,
-    ind.length = individual.body.size_length,
-    ind.width = individual.body.size_width,
-    ind.height = individual.body.size_height,
-    ind.depth = individual.body.size_depth,
-    ind.diameter = individual.body.size_diameter,
-    ind.dry.mass = `individual.body.size_dry mass`,
-    
-    multi.volume = multicellular.body.size_biovolume,
-    multi.length = multicellular.body.size_length,
-    multi.width = multicellular.body.size_width,
-    multi.height = multicellular.body.size_height,
-    multi.depth = multicellular.body.size_depth,
-    multi.diameter = multicellular.body.size_diameter,
-    multi.dry.mass = `multicellular.body.size_dry mass`,
-  ) 
-%>% 
-  
-  unnest( cols = everything()
-  ) %>% 
-  
-  mutate(
-    uid = paste(uid_biovolume, uid_length, uid_height, uid_width, uid_diameter, `uid_dry mass`, uid_depth, sep = ","),
-    uid = stri_replace_all_regex(uid, "NA,|NA|,NA", ""),
-    uid = na_if(uid, ""),
-    uid = as.numeric(stri_extract_first_regex(uid, "\\d+"))
-  ) %>% 
-  
-  select(
-    -starts_with("uid_")
-  ) %>% 
-  
-  left_join(
-    ., select(
-      bodysize_location, - body.size, - units, - bodysize.measurement, - individual.uid),
-    by = "uid"
-  ) %>% 
-  
-  select(
-    - life.stage,
-    - sample.year, # getting rid of dates because not need them for what I'm doing but will keep in raw data
-    - sample.month,
-    - sex, # only relevent for the zooplankton
-    - bodysize.measurement.notes, # don't need
-    - reps,
-    - sample.size,
-    - error,
-    - error.type
-  )
-  
-# get average from 
-x <- multiple_measurements %>% 
   filter(
-    is.na(sample.year)
-  )
-
-group_by(
-  individual.uid, sample.year, bodysize.measurement, location.code
-) %>% 
-  
-  summarise(
-    individual.body.size = mean(individual.body.size),
-    multicellular.body.size = mean(multicellular.body.size)
-  )
-  
-  # get averages from raw data so all are averages
-  
-  group_by(taxa.name, source.code) %>% 
-  
-  mutate(
-    volume = if_else(
-      measurement.type == "raw",
-      mean(volume),
-      volume 
-    )
-  ) 
-
-raw <- multiple_measurements %>% 
-  filter(
-    measurement.type == "raw"
-  )
-%>% 
-  
-  mutate(
-    mass = volume*(1*10^-6)
-  ) %>% 
-  
-
-
-  
-  ## Averages for multiple indivdual.uid measurments
-  # some papers gave averages for each individual and some gave raw so get averages for ones that are raw
-  group_by(
-    individual.uid
-  ) %>% 
-  
-  mutate(
-    mass = mean(mass)
-  ) %>% 
-  
-  distinct(individual.uid, .keep_all = TRUE) %>% 
-  
-  ungroup() %>% 
-  
-  mutate(
-    group = case_when(
-      bodysize.measurement == "biovolume" ~ "phytoplankton",
-      TRUE ~ "zooplankton"
-    )
-  ) %>% 
-  
-  relocate(
-    uid, individual.uid, tax.uid, source.code, original.sources, accepted.taxa.name, form, form.no, life.stage, sex, mass, min.body.size, max.body.size, body.size, units, bodysize.measurement, sample.size, error, error.type, sample.year, sample.month
+    rank == "Species"
   )
 
 # save
-saveRDS(bodysize_data, file = "R/Data_outputs/full_database/bodysize_data.rds")
+saveRDS(species_raw_cell_size, file = "R/Data_outputs/species_raw_cell_size.rds")
+
+# Species average ----
+
+# Calculate the average size per species
+species_average_cell_size <- species_raw_cell_size %>% 
+  
+  # select just species level
+  filter(
+    rank == "Species",
+    !is.na(mass)
+  ) %>% 
+  
+  # calculate average mass, biovolume, mld
+  group_by(tax.uid, nu) %>%  # want seperate values for single and multi-cellular
+  
+  summarise(
+    mass.mean = mean(mass, na.rm = TRUE),
+    biovolume.mean = mean(biovolume, na.rm = TRUE),
+    mld.mean = mean(mld, na.rm = TRUE),
+    n = n(),
+    mass.sd = sd(mass),
+    mass.se = mass.sd/sqrt(n),
+    .groups = "drop"
+  ) %>% 
+  
+  # change NaN to NA
+  mutate(
+    across(
+      everything(), ~ ifelse(
+        is.nan(.), NA_integer_, .
+        )
+      )
+    ) %>% 
+  
+  # add back in extra data
+  left_join(
+    tax_list, by = "tax.uid"
+  ) %>% 
+  
+  left_join(
+    kruk_groups, by = "taxa.name"
+  ) %>% 
+  
+  select(
+    taxa.name, 
+    expert.reynolds,
+    nu, mass.mean, biovolume.mean, mld.mean, 
+    n, mass.se, 
+    tax.uid, genus, family, order, class, phylum, kingdom
+  )
+
+# save
+saveRDS(species_average_cell_size, file = "R/Data_outputs/species_average_cell_size.rds")
+
+
--- R/Scripts/Creating_database/8_final_lists.R
+++ R/Scripts/Creating_database/8_final_lists.R
@@ -1,43 +1,151 @@
 # make final sources and tax list now all the ones i dont want have been taken out
 
-# for location check for ones that aren't present in the the main data anymore and remove
 
-# Check for unused ----
-# check for any locations that are in the location_raw sheet but not used in the main data and if so remove them
-location_check <- bodysize_location %>% 
-  
-  # Select columns
-  select(
-    location.code.1:location.code.17
+# Packages 
+library(here)
+library(readxl)
+library(tidyr)
+library(tidyverse)
+library(stringi)
+library(data.table)
+
+# Data ----
+species_raw_cell_size <- readRDS("R/Data_outputs/species_raw_cell_size.rds")
+tax_list <- readRDS("R/Data_outputs/taxonomy/gbif/tax_list.rds")
+source_list <- readRDS("R/Data_outputs/locations_sources/source_list.rds")
+location_list <- readRDS("R/Data_outputs/locations_sources/location_list.rds")
+
+# taxonomy list ----
+
+# get a list of all the distinct tax.uids still in the data
+tax_uids <- species_raw_cell_size %>% 
+  distinct(
+    tax.uid
   ) %>% 
   
-  # Pivot to get location.codes on seperate lines 
-  pivot_longer(., cols = 1:17, values_to = "location.code")%>% 
+  # convert to string
+  pull(tax.uid)
+
+# select just ones in the tax_uids list
+taxonomy <- tax_list %>% 
+  
+  filter(
+    tax.uid %in% tax_uids
+  ) %>% 
+  
+  # select and reorder - don't need type or rank as all are species and phyto, don't need resolved.taxa.name anymore
+  relocate(
+    tax.uid, taxa.name, species, genus, family, order, class, phylum, kingdom
+  )
+
+# save
+saveRDS(taxonomy, "R/data_outputs/taxonomy.rds")
+
+
+# location list ----
+
+# get a list of locations used in the final data
+location_codes <- species_raw_cell_size %>% 
+  
+  # select column
+  select(
+    location.code
+  ) %>% 
+  
+  # separate out into one column per code
+  separate(location.code, into = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"), sep = ";") %>% 
+  
+  # pivot onto their own rows
+  pivot_longer(
+    cols = 1:14,
+    values_to = "location.code"
+  ) %>% 
+  
+  # get all distinct non NAs
+  distinct(
+    location.code
+  ) %>% 
   
   filter(
     !is.na(location.code)
   ) %>% 
   
-  # get distinct ones
-  distinct(location.code) %>% 
+  # pull out into a vector
+  pull(location.code)
   
-  # anti_join to leave any that are in locations raw but not bodysize_location
-  anti_join(location_raw, ., by = "location.code")
+# select locations in location_codes
+location <- location_list %>% 
+  filter(
+    location.code %in% location_codes
+  )
 
-# Final location list ----
-# get final sheet by getting distinct location.codes - might need to be changed after later steps incase they remove any more data points
-location_list <- location_raw %>% 
+# save
+saveRDS(location, "R/data_outputs/location.rds")
+
+# Sources ----
+
+# get a list of sources used in final data
+source_codes <- species_raw_cell_size %>% 
+  
+  # select columns
+  select(
+    source.code,
+    original.sources
+    ) %>% 
+  
+  # separate out into their own columns
+  separate(original.sources, into = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"), sep = ";") %>% 
+  
+  # make same type
+  mutate(
+    across(
+      everything(), ~ as.integer(.)
+      )
+    ) %>% 
+  
+  # put onto their own rows 
+  pivot_longer(
+    cols = 1:17,
+    values_to = "source.code"
+  ) %>% 
+  
+  # get all distinct non NAs
+  distinct(
+    source.code
+  ) %>% 
   
   filter(
-    !(location.code %in% c("loc-121", "loc-135", "loc-"))
+    !is.na(
+      source.code
+    )
+  ) %>% 
+  
+  # pull out into a vector
+  pull(
+    source.code
   )
-
-distinct(location.code, .keep_all = TRUE) %>% 
+  
+# Select sources that are in source_codes
+sources <- source_list %>% 
+  
+  # rename new.source.code and source.code and remove old source.code
   select(
-    - join.location,
-    - source.code
+   - source.code
+  ) %>% 
+  
+  rename(
+    source.code = new.source.code
+  ) %>% 
+  
+  # select ones in source_codes
+  filter(
+    source.code %in% source_codes
+  ) %>% 
+  
+  # select and reorder
+  select(
+    source.code, authors, year, title, publication, volume, issue, start.page, end.page, doi, ISSN, ISBN
   )
-
-# save - list of all distinct locations used in the data
-saveRDS(location_list, file = "R/Data_outputs/full_database/location_list.rds")
-
+  
+# save
+saveRDS(sources, "R/data_outputs/sources.rds")
